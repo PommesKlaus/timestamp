f26436447aa888c571b0b3b3ed7c87e6b3b7a330
pommesklaus <christianfritz.mail@gmail.com>
2016-08-17 15:37:41 +0000

    finalized time server

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 var express = require('express');
+var chrono = require("chrono-node");
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/:date', function (req, res) {
+    var output = {
+        "unix": null,
+        "natural": null
+    }
+    var d = req.params.date;
+    var timereq = chrono.parseDate(d);
+    
+    if (timereq === "Invalid Date" || timereq === null) {
+        timereq = new Date(parseInt(d) * 1000);
+    }
+    
+    if (timereq != "Invalid Date" && timereq !== null) {
+        output.unix = timereq.getTime()/1000;
+        
+        var options = { year: 'numeric', month: 'long', day: 'numeric' };
+        output.natural = timereq.toLocaleDateString('de-DE', options);
+    }
+  
+    res.send(JSON.stringify(output));
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+    console.log('Timestamp app listening on port 8080!');
 });
